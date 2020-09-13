538e287a023719677da50ff2bc4307ab681524b7
seanwayland <seanwayland@gmail.com>
2020-09-13 16:22:03 -0400

    initial commit of header with buffer passed into write function

--- DelayProcessor.h
+++ DelayProcessor.h
@@ -1,1 +1,45 @@
 
+/*
+  ==============================================================================
+
+    A simple editor for the delay plugin
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "TapeDelayProcessor.h"
+
+
+//==============================================================================
+/**
+*/
+class TapeDelayAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    TapeDelayAudioProcessorEditor (TapeDelayAudioProcessor&);
+    ~TapeDelayAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    TapeDelayAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TapeDelayAudioProcessorEditor)
+
+    Slider mGainSlider      { Slider::RotaryHorizontalVerticalDrag,  Slider::TextBoxBelow };
+    Slider mTimeSlider      { Slider::RotaryHorizontalVerticalDrag,  Slider::TextBoxBelow };
+    Slider mFeedbackSlider  { Slider::RotaryHorizontalVerticalDrag,  Slider::TextBoxBelow };
+
+    AudioProcessorValueTreeState::SliderAttachment mGainAttachment      { processor.getValueTreeState(), TapeDelayAudioProcessor::paramGain,     mGainSlider };
+    AudioProcessorValueTreeState::SliderAttachment mTimeAttachment      { processor.getValueTreeState(), TapeDelayAudioProcessor::paramTimeOne,     mTimeSlider };
+    AudioProcessorValueTreeState::SliderAttachment mFeedbackAttachment  { processor.getValueTreeState(), TapeDelayAudioProcessor::paramFeedbackOne, mFeedbackSlider };
+};
